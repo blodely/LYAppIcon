f1e7845200c2662fd884e48089bbcc857fcb2d53
Luo Yu <luoyu@luoyu.space>
2018-07-19 15:30:05 +0800

    [Add] project

---
+++ iOSAppIcon/iOSAppIcon/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  iOSAppIcon
+//
+//  Created by Luo Yu on 2018/7/19.
+//  Copyright © 2018 Luo Yu. All rights reserved.
+//  Mozilla Public License Version 2.0
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+		
+	}
+	return 0;
+}
